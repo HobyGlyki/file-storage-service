ae14ec72fb3a9c03fcafd8d545f641c2c42943ea
HobyGlyki <hobegoblin14.87@gmail.com>
2026-03-27 05:34:03 +0300

    feat(docker): создание database.py с тестами базы данных

---
+++ serverlogic/database.py
@@ -0,0 +1,69 @@
+from sqlalchemy import Column, Integer, String, JSON, DateTime, create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, as_declarative
+import datetime
+import os
+
+#для тестов в оперативной памяти
+DATABASE_URL = "sqlite:///:memory:"
+
+
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+@as_declarative() #декоратор для класса
+class AbstractModel:
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    test_time = Column(DateTime, default=datetime.datetime.utcnow) #для тестов проверка создания данных.
+
+class Users(AbstractModel):
+    __tablename__ = "Client"
+    client_name = Column(String)
+    hash = Column(String)
+
+class FileServ(AbstractModel):
+    __tablename__ = "Schemas"
+
+    filename = Column(String)
+    file_type = Column(String)
+    minio_path = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow) 
+    metadata_json = Column(JSON)
+    
+
+    
+
+def init_db(): #инициализация таблицы.
+    AbstractModel.metadata.create_all(bind=engine)
+
+
+def timetest():
+    db = SessionLocal()
+    if not db.query(Users).first():
+        new_table = Users()
+        db.add(new_table)
+        db.commit()
+    
+    result = db.query(Users).first()
+    db.close
+    return result
+
+init_db()
+print(timetest().test_time)
+
+    
+
+def localtest():
+    init_db()
+
+    db = SessionLocal()
+
+    new_user = Users(
+            client_name="Тест",
+            hash="ХэшТест"
+        )
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    print(new_user.id)
