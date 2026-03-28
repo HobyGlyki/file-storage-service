b3c5805ef76772727ee1a905e6caee69617d7efd
HobyGlyki <hobegoblin14.87@gmail.com>
2026-03-28 03:27:42 +0300

    feat(zip): добавил распаковку архива в памяти, так же проверка наличия json файла и xsd файла.
    В будущем планирую сделать проверку JSON файла и уже интеграцию с Minio, загрузку файлов в него.

--- serverlogic/main.py
+++ serverlogic/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, File, UploadFile
 from serverlogic.database import *
 from fastapi.responses import FileResponse, HTMLResponse
+import os
+from zipfile import ZipFile
+from io import BytesIO
+import json
 
 init_db()
 
@@ -8,10 +12,35 @@ app = FastAPI()
 
 @app.get("/")
 async def index():
-    # Путь к HTML внутри папки Wikis
     return FileResponse('client/index.html')
 
 @app.post('/upload', response_class=HTMLResponse)
 async def upload_file(file: UploadFile):
-    return f'<p>{str(file.filename)}</p>'
+    #Zip файлы
+    if file.filename[-3:] == "zip":
+        print(True)
 
+        result =[]
+        jsonfile = None
+
+        byte = await file.read()
+        zfiledata = BytesIO(byte)
+        
+        with ZipFile(zfiledata, "r") as myzip:
+            for f in myzip.namelist(): 
+                if  f[-4:] == "json":
+                    jsonfile = f
+                else:
+                    result.append(f)
+
+            if not jsonfile:
+                return f'<p>невернывй тип файла, загрузити XLS или zip с json файлом </p>'
+            
+            resultjson = json.loads(myzip.read(jsonfile))
+            print(resultjson)
+            return f'<p>Файлы что хранятся:{str(result)} </p> <p>json файл:{jsonfile} </p>'
+        
+    if file.filename[-3:] == "xsd":
+        return f'<p> файл xsd:{file.filename}</p>'
+    else:
+        return f'<p>невернывй тип файла, загрузити xsd или zip с json файлом </p>'
